12bcac113ae30ffea2ed291e6f79ee5222dee363
ljdongs <ljdongs@126.com>
2019-05-21 21:18:39 +0800

    main.js修改提交

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(num1, num2) {
+  if (num2 == 0)
+  {
+  alert('数据不能为0')
+  }
+  else {
+  var num = num1 % num2
+  }
+  return num
 };
